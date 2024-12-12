6b79a8028573444cef10ceb71bf95eb30c7dd603
naresharin <arin.working@gmail.com>
2024-12-12 16:25:43 +0530

    New

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js"
-
+import { app } from './app.js'
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`${process.env.USER} is running server at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB Connection Failed: ",err)
+})
  
