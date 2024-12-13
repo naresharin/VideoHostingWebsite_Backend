ef0023757fe5c498c4b1093d3cfdcc18598540d2
naresharin <arin.working@gmail.com>
2024-12-13 16:29:07 +0530

    New

--- src/app.js
+++ src/app.js
@@ -16,4 +16,13 @@ app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// Routes Import
+import userRouter from './routes/user.routes.js'
+
+
+//Routes Declaration
+app.use("/api/v1/users", userRouter)
+
+
 export { app };
