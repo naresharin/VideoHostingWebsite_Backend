f3ca5a5ab259099c2d70eb2395b4fdc90fb9a074
naresharin <arin.working@gmail.com>
2024-12-14 01:24:33 +0530

    New

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type:'auto'
         })
         console.log(`File uploaded Successfully:`,response.url);
-
+        fs.unlinkSync(localFilePath)
         return response
         
     } catch (error) {
