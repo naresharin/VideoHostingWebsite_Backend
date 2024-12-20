7552dd6a699943ded7f9e64d95eda4d3553b36b9
naresharin <arin.working@gmail.com>
2024-12-20 12:37:45 +0530

    New

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,5 +1,3 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
@@ -8,17 +6,45 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    // TODO: toggle subscription
+    
+    const existingSubs = await Subscription.findOne({subscriber:req.user,channel:channelId})
+
+    let subscribe
+    if(existingSubs){
+        subscribe = await existingSubs.deleteOne()
+    } else {
+        subscribe = await Subscription.create({subscriber:req.user,channel:channelId})
+    }
+
+    if(!subscribe) throw new ApiError(400,"Could not toggle subscribe")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(201,subscribe,"Subscribe toggled succesfully"))
+
+    
 })
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    const subscribers = await Subscription.find({channel:channelId})
+    if(!subscribers) throw new ApiError(400,"No Subscribers")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(201, subscribers, "Subscribers fetched successfully"))
 })
 
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    const channels = await Subscription.find({subscriber:subscriberId})
+    if(!channels) throw new ApiError(400,"No Channels Subscribed")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(201, channels, "Subscribed Channels fetched successfully"))
 })
 
 export {
